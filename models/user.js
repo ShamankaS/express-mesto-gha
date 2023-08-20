af2b3b49c1fb504f525200a6cc17efd437269a5d
Tata Slyadneva <79031563664@ya.ru>
2023-08-20 18:59:57 +0300

    feat: updated models

--- models/user.js
+++ models/user.js
@@ -1,22 +1,28 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const UnauthorizedError = require('../utils/errors/unauthorized-err');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     default: 'Жак-Ив Кусто',
-    minlength: [2, 'Минимальная длина поля 2'],
-    maxlength: [30, 'Максимальная длина поля 30'],
+    minlength: [2, 'Минимальная длина поля 2 символа'],
+    maxlength: [30, 'Максимальная длина поля 30 символов'],
   },
   about: {
     type: String,
     default: 'Исследователь',
-    minlength: [2, 'Минимальная длина поля 2'],
-    maxlength: [30, 'Максимальная длина поля 30'],
+    minlength: [2, 'Минимальная длина поля 2 символа'],
+    maxlength: [30, 'Максимальная длина поля 30 символов'],
   },
   avatar: {
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator: (v) => validator.isURL(v, { protocols: ['http', 'https'], require_protocol: true }),
+      message: ({ value }) => `${value} - некоректный адрес URL. Ожидается адрес в формате: http(s)://(www).site.com`,
+    },
   },
   email: {
     type: String,
@@ -33,5 +39,23 @@ const userSchema = new mongoose.Schema({
     select: false,
   },
 });
+
+userSchema.statics.findUserByCredentials = async function (email, password) {
+  try {
+    const user = await this.findOne({ email }).select('+password');
+    if (!user) {
+      return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
+    }
+    const matched = await bcrypt.compare(password, user.password);
+    if (!matched) {
+      return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
+    }
+    if (user && matched) {
+      return user;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = mongoose.model('user', userSchema);
